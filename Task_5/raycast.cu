5cf560e3d0822c66aa6ebfcaf8190d7ed02a89c5
Julian Ho-Yin Lam <jhlam@gpu04.(none)>
2014-10-29 16:45:54 +0100

    Change to follow the example on the net.

--- Task_5/raycast.cu
+++ Task_5/raycast.cu
@@ -330,8 +330,8 @@ __global__ void raycast_kernel(unsigned char* data, unsigned char* image, unsign
 //The part down here is the part where the camera go through the #D datasheet and sample the values.
 	//the code would be the same, but the differents would be that the double for-loop would be just for this single thread. 
 	
-	int x = threadIdx.x - (IMAGE_DIM/2);
-	int y = blockDim.y - (IMAGE_DIM/2);
+	int x = (blockIdx.x*blockDim.x + threadIdx.x) - (IMAGE_DIM/2);
+	int y = (blockIdx.y*blockDim.y+ threadIdx.y)  - (IMAGE_DIM/2);
 	float3 screen_center = add(camera, forward);
 	float3 ray = add(add(screen_center, scale( right, x*pixel_width)), scale(up, y*pixel_width));
 	ray = add(ray, scale(camera, -1));
